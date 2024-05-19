d472a9c2bb18d8a779b2d18d0cadeb4b88b57d3f
Maksym <s28493@pjwstk.edu.pl>
2024-05-20 01:19:06 +0200

    zrobione zadanie

---
+++ LinqTutorials/LinqTasks/Extensions/CustomExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using LinqTasks.Models;
+
+namespace LinqTasks.Extensions;
+
+public static class CustomExtensionMethods
+{
+    //Put your extension methods here
+    public static IEnumerable<Emp> GetEmpsWithSubordinates(this IEnumerable<Emp> emps)
+    {
+        return emps
+            .Where(e => emps.Any(sub => sub.Mgr?.Empno == e.Empno))
+            .OrderBy(e => e.Ename)
+            .ThenByDescending(e => e.Salary);
+    }
+}
